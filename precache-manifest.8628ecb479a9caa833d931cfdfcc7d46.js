8a93763e5098eb6ef7002827c22fcd8859e59290
Min Zhang <joye@mins-mbp.home>
2021-01-28 21:52:55 -0800

    Updates

---
+++ precache-manifest.8628ecb479a9caa833d931cfdfcc7d46.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a530c2d881d26d039865edc692ff1840",
+    "url": "/joyecs.github.io/index.html"
+  },
+  {
+    "revision": "caaeb2db4b96bf724cef",
+    "url": "/joyecs.github.io/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "174fb1223d15e0509b3c",
+    "url": "/joyecs.github.io/static/css/main.cd54a8f5.chunk.css"
+  },
+  {
+    "revision": "caaeb2db4b96bf724cef",
+    "url": "/joyecs.github.io/static/js/2.d7b3b9e3.chunk.js"
+  },
+  {
+    "revision": "2ad133169a5e043fc6c51b3f01b52cab",
+    "url": "/joyecs.github.io/static/js/2.d7b3b9e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3bff3ac6fb45cf69b02",
+    "url": "/joyecs.github.io/static/js/3.fd6ffb1f.chunk.js"
+  },
+  {
+    "revision": "174fb1223d15e0509b3c",
+    "url": "/joyecs.github.io/static/js/main.bd059c65.chunk.js"
+  },
+  {
+    "revision": "884ef04a224312a911c7",
+    "url": "/joyecs.github.io/static/js/runtime-main.20af17ec.js"
+  },
+  {
+    "revision": "60bed0f8fefeb7ca10671f11d96d531f",
+    "url": "/joyecs.github.io/static/media/Resume.60bed0f8.pdf"
+  },
+  {
+    "revision": "8a717b33614f79c258f7e274fecc1d4e",
+    "url": "/joyecs.github.io/static/media/card4.8a717b33.jpg"
+  },
+  {
+    "revision": "e190db0f36520f59f5c3d0d12f8f899a",
+    "url": "/joyecs.github.io/static/media/dream.e190db0f.jpg"
+  },
+  {
+    "revision": "2de6018cb1ebea49982284b1f9a1dd0c",
+    "url": "/joyecs.github.io/static/media/frontend.2de6018c.png"
+  },
+  {
+    "revision": "7343653b45fc4151ae0eb7e72ab19981",
+    "url": "/joyecs.github.io/static/media/profile2.7343653b.jpeg"
+  }
+]);
