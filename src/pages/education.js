169400053ab4bba1e13dba9adfac9bc65e66c6e8
joyecs <joyee816@gmail.com>
2022-12-07 20:48:54 -0800

    update education

--- src/pages/education.js
+++ src/pages/education.js
@@ -10,21 +10,21 @@ function Education(props){
         <div className="container text-left">
             <strong>   <FontAwesomeIcon icon={faGraduationCap}/> &nbsp; California State University, San Bernardino  &nbsp; &nbsp;
             <FontAwesomeIcon icon={faSearchLocation}/> &nbsp;MI, USA</strong>      
-            <br></br>Degree and Field: MS IST 2021-2023
+            <br></br>Degree and Field: Master of Science, MS - Information Systems and Technology 2021-2023
             <br></br>Courses: Python, Database Structure, Cyber Security.
             <br></br> GPA: <span className="badge badge-success">Excellent</span> <br></br><br></br>
             <ProgressBar animated now={90} variant='success'/> 
             <hr></hr>
             <strong>   <FontAwesomeIcon icon={faGraduationCap}/> &nbsp; Andrews University  &nbsp; &nbsp;
             <FontAwesomeIcon icon={faSearchLocation}/> &nbsp;MI, USA</strong>      
-            <br></br>Degree and Field: MBA 2018-2019
+            <br></br>Degree and Field: Master of Business Administration, MBA 2018-2019
             <br></br>Courses: Leadership, Marketing, Economics, Financial Management, etc.
             <br></br> GPA: <span className="badge badge-success">Excellent</span> <br></br><br></br>
             <ProgressBar animated now={98} variant='success'/> 
             <hr></hr>
             <strong>  <FontAwesomeIcon icon={faGraduationCap}/>&nbsp; Guangzhou Medical University  &nbsp; &nbsp;
             <FontAwesomeIcon icon={faSearchLocation}/>  &nbsp;Guangzhou, CN</strong>
-            <br></br>Degree and Field: Infomation Management and Information System     
+            <br></br>Degree and Field: Bachelor of Science, BS - Computer Science    
             <br></br>Courses: C#, Java, Database, Operation System, Network Security, etc.  
             <br></br>GPA: <span className="badge badge-info">Good</span> <br></br><br></br>
             <ProgressBar animated now={75} variant="info"/> 
