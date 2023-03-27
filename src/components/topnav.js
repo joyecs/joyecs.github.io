d1a32b622ba68f5091fcdadeba614c6fbda4d562
joyecs <joyee816@gmail.com>
2023-03-26 21:40:52 -0700

    browser router

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -17,7 +17,7 @@ function TopNav() {
         <Nav className="mr-auto">
           {/* <Nav.Link href="mailto:joyee816@gmail.com">joyee816@gmail.com</Nav.Link>
           <Nav.Link href="#">2693620864</Nav.Link> */}
-          <Nav.Link href='/#/education'>
+          <Nav.Link href='/education'>
             Education
           </Nav.Link>
           <Nav.Link href="/experience">Experience</Nav.Link>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  BrowserRouter,
-  createHashRouter,
-  HashRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   BrowserRouter,
+//   createHashRouter,
+//   HashRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import Switch from 'react-bootstrap/esm/Switch';
+import {BrowserRouter as Router, } from 'react-router-dom';
+import {  Switch, Route } from 'react-router';
 import Home from './pages/home';
 import Education from './pages/education';
 import Experience from './pages/experience';
@@ -18,39 +21,54 @@ import Detail from './components/duty';
 import Blog from './pages/blog';
 import ErrorElement from './components/error';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorElement />,
-  },
-  {
-    path: "/#/education",
-    element: <Education />,
-  },
-  {
-    path: "/experience",
-    element: <Experience />,
-  },
-  {
-    path: "/portfolio",
-    element: <Portfolio />,
-  },
-  {
-    path: "/portfolio/:project_name",
-    element: <Detail />,
-  },
-  {
-    path: "/blog",
-    element: <Blog />,
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//     errorElement: <ErrorElement />,
+//   },
+//   {
+//     path: "/#/education",
+//     element: <Education />,
+//   },
+//   {
+//     path: "/experience",
+//     element: <Experience />,
+//   },
+//   {
+//     path: "/portfolio",
+//     element: <Portfolio />,
+//   },
+//   {
+//     path: "/portfolio/:project_name",
+//     element: <Detail />,
+//   },
+//   {
+//     path: "/blog",
+//     element: <Blog />,
+//   }
+// ]);
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <RouterProvider
+    <Router >
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/education" component={Education} />
+        <Route path="/experience" component={Experience} />
+        <Route path="/portfolio" component={Portfolio} />
+
+        <Route
+          path="/portfolio/:project_name"
+          render={({ match }) => (
+            <Detail project_name={match.params.project_name} />
+          )}
+        />
+      </Switch>
+    </Router>
+    {/* <RouterProvider
       router={router}
-    />
+    /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
